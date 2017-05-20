8f9e8a42083d611c776b0e17252742b72e7d2c8b
Seth Lopez <sethlopezme@users.noreply.github.com>
2017-05-20 16:36:43 -0400

    chore: Remove flowtype

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,22 +8,15 @@ module.exports = {
   globals: {
     atom: true,
   },
-  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module',
   },
-  plugins: ['flowtype'],
   rules: {
     indent: ['error', 2, { SwitchCase: 1 }],
     'linebreak-style': ['error', 'unix'],
     'no-console': ['off'],
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
-  },
-  settings: {
-    flowtype: {
-      onlyFilesWithFlowAnnotation: false,
-    },
   },
 };
