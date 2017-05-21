a9077f243333655dc4829a50bcbf8e015e870e78
Seth Lopez <sethlopezme@users.noreply.github.com>
2017-05-20 21:37:24 -0400

    Add docblocks to Notifier methods

--- src/notifier.js
+++ src/notifier.js
@@ -3,6 +3,12 @@ const defaults = {
   packageName: 'atom-rust',
 };
 
+/**
+ * Display an error notification to the user with a button to create an issue.
+ *
+ * @param   {string}  title         The title of the notification.
+ * @param   {Object}  [options={}]  Notification options.
+ */
 function error(title, options = {}) {
   let config;
 
@@ -18,16 +24,34 @@ function error(title, options = {}) {
   atom.notifications.addFatalError(title, config);
 }
 
+/**
+ * Display a normal notification to the user.
+ *
+ * @param   {string}  title         The title of the notification.
+ * @param   {Object}  [options={}]  Notification options.
+ */
 function info(title, options = {}) {
   let config = Object.assign({}, defaults, options);
   atom.notifications.addInfo(title, config);
 }
 
+/**
+ * Display a success notification to the user.
+ *
+ * @param   {string}  title         The title of the notification.
+ * @param   {Object}  [options={}]  Notitication options.
+ */
 function success(title, options = {}) {
   let config = Object.assign({}, defaults, options);
   atom.notifications.addSuccess(title, config);
 }
 
+/**
+ * Display a warning notification to the user.
+ *
+ * @param   {string}  title         The title of the notitication.
+ * @param   {Object}  [options={}]  Notification options.
+ */
 function warning(title, options = {}) {
   let config = Object.assign({}, defaults, options);
   atom.notifications.addWarning(title, config);
